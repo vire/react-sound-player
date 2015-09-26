522c38fd4a80d132475e77e0ffeb9ba1d3fe533e
Viliam Elischer <viliam.elischer@zoomint.com>
2015-09-26 12:24:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+React Sound Player
+=====================
+
+A simple react component that uses WebAudioAPI to play audio files. It's built on top of [react-transform-boilerplate](https://github.com/gaearon/react-transform-boilerplate.git)
+
+License: MIT
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SoundPlayer {
+  render() {
+    return (
+      <div>
+        Here goes the React Sound Player
+      </div>
+    )
+  }
+}
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <SoundPlayer>
+
+        </SoundPlayer>
+      </div>
+    );
+  }
+}
